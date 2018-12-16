52c2efcf560ecb3e96e3ee50d3f0a5c2b0e1a439
Saha <windcrack@mail.ru>
2018-12-16 17:26:37 +0300

    Lesson 3

---
+++ lesson 3/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import styled from 'styled-components';
+
+import Header from './components/header'
+import Menu from './components/menu'
+import Main from './components/main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height:600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
--- lesson 3/src/components/main.js
+++ lesson 3/src/components/main.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
 import Slider from './slider'
 
 
